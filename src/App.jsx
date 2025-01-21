7cc650f6a0e1627f688b2e35095fdcb0f2a96536
Akmal1796 <mhdakmal1796@gmail.com>
2025-01-21 22:31:41 +0530

    Finished the header section design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import Login from './components/Login'
-import Register from './components/Register'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
+    <Header />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
+import { MdOutlineMenu } from "react-icons/md";
+import { IoSearch } from "react-icons/io5";
+import { IoNotifications } from "react-icons/io5";
+import profile from "../Images/profile.png"
 
 const Header = () => {
   return (
-    <section className='flex justify-between items-center p-6 bg-white'>
-        <div>LOGO</div>
-        <div>MENU</div>
-        <div>Dashboard</div>
-        <div>Search bar</div>
-        <div>Notifications</div>
-        <div>Profile</div>
+    <section className='flex justify-between items-center p-6 w-full h-20 bg-white'>
+        <div className='flex justify-between items-center w-[20%]'>
+          <div className='font-semibold'>LOGO</div>
+          <div><MdOutlineMenu size={25} className='cursor-pointer' /></div>
+          <div className='font-bold'>Dashboard</div>
+        </div>
+        <div className='flex justify-center items-center w-[25%] relative overflow-hidden'>
+          <input type='text' placeholder='Search' className='rounded-md bg-slate-100 px-4 py-1 w-full' />
+          <div className='bg-slate-50 py-1.5 px-2 absolute -right-0.5 rounded-l-none rounded-r-md'><IoSearch className='cursor-pointer' size={20} /></div>
+        </div>
+        <div className='flex justify-around items-center w-[20%]'>
+          <div className='relative'>
+            <IoNotifications size={25} className='cursor-pointer'/>
+            <div className='bg-red-500 text-white text-[10px] rounded-full w-4 h-4 flex justify-center items-center absolute top-[-5px] right-[-3px] cursor-pointer'>13</div>
+          </div>
+          <div className='flex justify-end items-center'>
+            <div className='mx-3 text-right'>
+              <p>Mohamed Akmal</p>
+              <p className='text-gray-400'>Admin</p>
+            </div>
+            <img src={profile} alt='profile image' className=' w-10 h-10 rounded-full' />
+          </div>
+        </div>
     </section>
   )
 }
